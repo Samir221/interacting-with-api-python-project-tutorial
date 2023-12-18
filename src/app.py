c83f5dc1009eb140a58d50180171f5e7dede26fb
Samir <98545750+Samir221@users.noreply.github.com>
2023-12-18 18:30:50 +0000

    Added project code.

--- src/app.py
+++ src/app.py
@@ -1,15 +1,47 @@
-import os
-from sqlalchemy import create_engine
-import pandas as pd
+import requests
 from dotenv import load_dotenv
+import os
+import spotipy
 
-# load the .env file variables
+
+# Load environment variables from .env file
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+# Spotify API credentials
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+def get_spotify_token(client_id, client_secret):
+    """Get the access token from Spotify"""
+    url = "https://accounts.spotify.com/api/token"
+    payload = {'grant_type': 'client_credentials'}
+    headers = {'Authorization': f'Basic {client_id}:{client_secret}'}
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+    response = requests.post(url, headers=headers, data=payload)
+    return response.json().get('access_token')
+
+def get_top_tracks(artist_id, token):
+    """Get the top tracks of an artist from Spotify"""
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    headers = {'Authorization': f'Bearer {token}'}
+
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+# Replace with your Spotify Client ID and Client Secret
+client_id = CLIENT_ID
+client_secret = CLIENT_SECRET
+
+# Replace with the artist's Spotify ID
+artist_id = 'YOUR_ARTIST_ID_HERE'
+
+# Get access token
+token = get_spotify_token(client_id, client_secret)
+
+# Get top tracks
+top_tracks = get_top_tracks(artist_id, token)
+
+# Print the top 10 tracks
+for track in top_tracks['tracks'][:10]:
+    print(track['name'])
